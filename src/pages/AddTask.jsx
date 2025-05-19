7919b08919ee6271bb7ad1b7c73bea34642aca0d
michelelongo97 <m.longo7x@gmail.com>
2025-05-19 18:08:01 +0200

    feat: add milestone 5

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,7 +1,63 @@
+import { useMemo, useRef, useState } from "react";
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
+
 export default function AddTask() {
+  const [title, setTitle] = useState("");
+  const descriptionRef = useRef();
+  const statusRef = useRef();
+
+  const taskTitleError = useMemo(() => {
+    if (!title.trim()) return "Il nome della task non può essere vuoto";
+    if ([...title].some((char) => symbols.includes(char)))
+      return "Il nome della task non può contenere simboli";
+    return "";
+  }, [title]);
+
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    if (taskTitleError) return;
+
+    const newTask = {
+      title: title.trim(),
+      description: descriptionRef.current.value,
+      status: statusRef.current.value,
+    };
+
+    console.log(newTask);
+  };
+
   return (
     <div>
       <h1>Aggiungi Task</h1>
+      <form onSubmit={handelSubmit}>
+        <label>
+          Nome del task
+          <input
+            type="text"
+            placeholder="nome"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        {taskTitleError && <p style={{ color: "red" }}>{taskTitleError}</p>}
+        <br />
+        <label>
+          Descrizione
+          <textarea ref={descriptionRef} placeholder="descrizione" />
+          <br />
+        </label>
+        <label>
+          Stato
+          <select ref={statusRef} defaultValue={"To Do"}>
+            <option value="To Do">To Do</option>
+            <option value="Doing">Doing</option>
+            <option value="Done">Done</option>
+          </select>
+          <br />
+        </label>
+        <button type="submit">Aggiungi Task</button>
+      </form>
     </div>
   );
 }
