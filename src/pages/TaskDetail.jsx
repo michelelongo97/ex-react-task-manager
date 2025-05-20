1c1ae6e1d85e4543c03417f79b7e843b1825b44e
michelelongo97 <m.longo7x@gmail.com>
2025-05-20 12:31:11 +0200

    feat: add milestone 8

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { GlobalContext } from "../contexts/GlobalContext";
 
 export default function TaskDetail() {
   const { id } = useParams();
-  const { tasks } = useContext(GlobalContext);
+  const navigate = useNavigate();
+  const { tasks, removeTask } = useContext(GlobalContext);
 
   const task = tasks.find((task) => task.id === parseInt(id));
 
@@ -12,8 +13,15 @@ export default function TaskDetail() {
     return <h1>Task non trovata</h1>;
   }
 
-  const handleDelete = () => {
-    console.log(task.id);
+  const handleDelete = async () => {
+    try {
+      await removeTask(task.id);
+      alert("Task eliminata");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
   };
 
   return (
